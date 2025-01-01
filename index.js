4b9ad40714db32ba43705602e7af616ad01c7913
alinaasrzad <95015661+alinaasrzad@users.noreply.github.com>
2025-01-01 19:23:44 +0330

    up

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ console.log(name);
 // rowls
 // cannot be a reserved keyword 
 // should be meaningful
+//them cannot start number(nam1)
+// cannot contain a spacee or hyphen (_ -)
+//are case-sensitive
+let firstName = "alina" ;
+let  lastName =  "asrzad";
