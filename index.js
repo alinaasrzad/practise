6c141e90284ed5d7faf2fc40d3938af2fcf93964
alinaasrzad <95015661+alinaasrzad@users.noreply.github.com>
2025-01-02 21:05:23 +0330

    up

--- index.js
+++ index.js
@@ -15,20 +15,21 @@
  interestRate=5;
  console.log(interestRate)
  //primitves
-let name = 'alina';
-let age = 22;
-let isApproved = false;
- // bolean
-let firstname = undefined;
-let selectedColor = null;
-let person = {
-    name : 'alina',
-    age : 23,
-lastName : 'asrzad'
+// let name = 'alina';
+// let age = 22;
+// let isApproved = false;
+//  // bolean
+// let firstname = undefined;
+// let selectedColor = null;
+// let person = {
+//     name : 'alina',
+//     age : 23,
+// lastName : 'asrzad'
 
-};
-//bracket notation
-person['name'] = 'sara';
-//dot notation *
-person.lastName = 'sam  '
-console.log(person.name )
+// };
+// //bracket notation
+// person['name'] = 'sara';
+// //dot notation *
+// person.lastName = 'sam  '
+// console.log(person.name )
+let selectedColors = ['red', 'blue'];
